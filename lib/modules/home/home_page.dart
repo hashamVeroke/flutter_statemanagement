4850502fdb2eeb74a654acdf410efbbb7f880bf4
Ahmad Hasham <ahmad.hasham@veroke.com>
2025-09-16 01:50:58 -0700

    Update dependencies to include dio and mime packages, and adjust dio version in pubspec.yaml. Remove unused import in home_page.dart and change navigation method in splash_view.dart from go to replace.

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:bloc_example/bloc/counter_bloc.dart';
-import 'package:bloc_example/modules/home/inc_dec_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
--- lib/modules/splash/splash_view.dart
+++ lib/modules/splash/splash_view.dart
@@ -23,7 +23,7 @@ class _SplashViewState extends State<SplashView>
     // Listen for animation complete
     _controller.addStatusListener((status) {
       if (status == AnimationStatus.completed) {
-        context.go('/onboarding'); // GoRouter navigation
+        context.replace('/onboarding'); // GoRouter navigation
       }
     });
   }
