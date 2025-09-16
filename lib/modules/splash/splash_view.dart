59bb5b4ad05407c51191d01861f4049a857a262c
Ahmad Hasham <ahmad.hasham@veroke.com>
2025-09-16 00:41:35 -0700

    Refactor SplashView to use StatefulWidget and integrate GoRouter for navigation. Added animation controller for Lottie asset to manage splash screen animation lifecycle.

--- lib/modules/splash/splash_view.dart
+++ lib/modules/splash/splash_view.dart
@@ -1,9 +1,38 @@
 import 'package:bloc_example/utils/ui_util/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:go_router/go_router.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(vsync: this);
+
+    // Listen for animation complete
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        context.go('/home'); // GoRouter navigation
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +41,12 @@ class SplashView extends StatelessWidget {
       body: Center(
         child: Lottie.asset(
           'assets/lottie/splash_screen.json',
+          controller: _controller,
+          onLoaded: (composition) {
+            _controller
+              ..duration = composition.duration
+              ..forward();
+          },
         ),
       ),
     );
